c0801b10c16ebd13188852204f809474fa9b0935
dkswldms12 <57124884+dkswldms12@users.noreply.github.com>
2019-12-09 18:05:36 +0900

    1209

--- hh/1206java/js/script.js
+++ hh/1206java/js/script.js
@@ -74,7 +74,7 @@ document.write($no_01+"<br>");
 $no_01/=$no_02; //$no_01=$no_01/$no_02
 document.write($no_01+"<br>");
 $no_01%=$no_02; //$no_01=$no_01%$no_02
-document.write($no_01+"<br>");
+document.write($no_01+"<br>"+"------------------");
 
 var $t="<table border='1' style='border-collapse:collapse;'>";
 $t+="<tr>";
@@ -136,3 +136,145 @@ document.write($num_02 === "7");
 document.write($num_03 === 12);
 
 document.write($num_01 > $num_02 +"<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("20191209"+"<br>");
+//논리연산자 ||연산자->or 내부의 값이 하나라도 true면 전체의 값은 true
+var $a = 10;
+var $b = 12;
+var $c = 24;
+var $d = 21;
+
+var $result;
+
+$result = $a>$b || $c>$d;//true
+document.write($result+"<br>");
+$result = $a>$b && $c>$d;//false
+document.write($result+"<br>");
+$result = !($a>$b);//! not 연산자 기존의 값이 true라면 반대로 false를 반환
+document.write($result+"<br>");
+
+//삼항조건 연산자 조건문?  실행문1 : 실행문2 -> 조건식이 true면 1번을 false면 2번을 실행
+var $hour = 14;
+var $result = $hour>=12?"오후"+($hour-12)+"시":"오전"+$hour+"시";
+document.write($result+"<br>");
+
+//조건식 if(조건식){실행문}
+var $age=17;
+	if($age>=20){
+		document.write("당신은 성년입니다."+"<br>");
+	}else{
+		document.write("당신은 미자입니다."+"<br>");
+	}
+
+//var $age=17;
+/*var $age=prompt("당신의 나이는?","20");
+if($age>0){	
+	if($age>=20){
+		document.write("당신은 어른입니다."+"<br>");
+	}else if($age>=13&&$age<20){
+		document.write("당신은 청소년입니다."+"<br>");
+	}else{
+		document.write("당신은 아동입니다."+"<br>");
+	}
+}else{
+	alert("0보다 큰수로 다시입력해주세요");
+	location.reload();//강제 새로고침
+}
+
+//var $userName=prompt("이름을 입력하세요","");
+if($userName){//==true 생략
+	document.write($userName+"님 환영합니다.")
+}*/
+
+/*중첩 if:1차 if문의 조건에 부합할때 2차if문의 조건식을 검사하여 해당하는 곳의 실행문을 출력*/
+/*var $in_num= parseInt(prompt("네자리 이상의 수를 입력하세요.","0000"));
+console.log($in_num);
+if($in_num>=1000){
+	if($in_num%3==0){
+		document.write("3의 배수입니다");
+	}else{
+		document.write("3의 배수가 아닙니다.");
+	}
+}else{
+	alert("입려한 숫자가 유효하지 않습니다. 재입력 바람");
+	location.reload();
+}*/
+/*
+var $season = prompt("현재 월을 입력하세요","");
+if($season>=0 && $season<=12){
+	if($season>=3&&$season<=5){
+		document.write("봄!!")
+	}else if($season>=6&&$season<=8){
+		document.write("여름!!")
+	}
+	else if($season>=9&&$season<=11){
+		document.write("가을!!")
+	}else{
+		document.write("겨울!!")
+	}
+}else{
+	alert("0보다 큰수 입력해요");
+	location.reload();
+}
+*/
+
+/*
+document.write("<br>");
+var $month=parseInt(prompt("현재 월을 입력하세요",""));
+if($month>=1&&$month<=12){
+	document.write("<img src='img/m_"+$month+".png' alt='"+$month+"월'>"+"<br>");
+}
+
+//if문을 활용한 로그인 구성하기
+var $user_id="dkswldms";
+var $user_pw="1234";
+
+var $in_id=prompt("아이디를 입력하세요","");
+var $in_pw=prompt("비밀번호를 입력하세요","");
+
+if($user_id==$in_id&&$user_pw==$in_pw){
+	document.write($in_id+"님의 방문을 환영합니다")
+}else{
+	alert("다시입력하세요");
+	location.reload();
+}
+
+// switch : switch~case+break....default
+var $select=prompt("자주사용하는 포털 사이트는 어디인가요? \n조건:네이버, 다음, 구글 중에서 선택하여 입력하시오","");
+var $url;//undefined 데이터
+switch($select){
+	case "네이버" : $url="https://www.naver.com";
+	break;
+	case "구글" : $url="https://www.google.com";
+	break;
+	case "다음" : $url="https://www.daum.net";
+	break;
+	default : alert("조건에 일치하는 사이트를 입력하세요");
+}
+if($url){
+	location.href=$url;
+}*/
+
+var $session=prompt("관심있는 있는 분야의 세션 번호를 선택하세요. \n 힌트 : 1-디자인, 2-개발파트, 3-마케팅","");
+console.log(typeof $session);
+//문자형 데이터를 숫자형 데이터로 변경 : number("문자형 데이터") var num=Number("7");
+switch($session){
+	case "1" : document.write("<img src='img/img_01.png'<p>세미나실 201호</p>"+"<br>");
+	break;
+	case "2" : document.write("<img src='img/img_02.png'<p>세미나실 202호</p>"+"<br>");
+	break;
+	case "3" : document.write("<img src='img/img_03.png'<p>세미나실 203호</p>"+"<br>");
+	break;
+	default : alert("조건에 일치하는걸 입력하세요");
+	location.reload();
+}
+
+//반복문은 세가지 필수항목 1 초기값 2 조건문 또는 조건식 3 증감식+실행문
+document.write("<h3>while 문</h3>");
+var $n=1;
+while($n<=3){
+	document.write("<div class='box box"+$n+"'><img src='img/img_0"+$n+".png'></div>");
+	$n++;
+}
+
