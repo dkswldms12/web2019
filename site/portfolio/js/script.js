41d5b1b4f803bd3320b2c7f1b11c3f86c5dbfc57
dkswldms12 <57124884+dkswldms12@users.noreply.github.com>
2020-01-07 18:52:59 +0900

    010701070107

--- site/portfolio/js/script.js
+++ site/portfolio/js/script.js
@@ -11,7 +11,7 @@ for(i=1; i<=12; i++){
 }
 */
 
-var text = "배운 사람은 항상 자기 속에 재산이 있다. - 필래드라스 -";
+var text = "배운 사람은 항상 자기 속에 재산이 있다.<br> - 필래드라스 -";
 var cnt = 0;
 var speed = 150;//글자가 찍히는 속도
 var timer1 = null;
@@ -144,6 +144,86 @@ $(document).ready(function(){
 		]
 	});
 
+
+	$(".headerT .res_btn a").click(function(){
+		var $active = $(".headerT .menu").hasClass("active");
+		if($active == true){
+			$(".headerT .menu").removeClass("active");
+			$(".headerT .menu").slideUp();
+		}else{
+			$(".headerT .menu").addClass("active");
+			$(".headerT .menu").slideDown();
+		}
+		return false;
+	});
+
+
+
+	function resize_ev(){
+		var $cur_width = $(window).width();
+		if($cur_width<768){
+			$(".menu").addClass("res_menu");
+			$(".menu").removeClass("pc_menu");
+			$(".menu").hide();
+
+		}else{
+			$(".menu").addClass("pc_menu");
+			$(".menu").removeClass("res_menu");
+			$(".menu").show();
+		}
+		$(".sub_m").hide();
+	};
+
+	resize_ev();
+
+
+	$(window).resize(function(){
+
+		resize_ev();
+	});
+
+
+//클릭 이벤트 영역(모바일 반응형)
+
+	$(".menu>ul>li").click(function(){
+		var $has = $(this).hasClass("active");
+		if($has == true){
+			$(".menu>ul>li").removeClass("active");
+			$(".menu>ul>li>.sub_m").slideUp();
+		}else{
+			$(this).addClass("active");
+			$(".menu>ul>li>.sub_m").slideUp();
+			$(this).find(".sub_m").slideDown();
+		}
+		return false;
+
+
+	});
+
+	$(".res_btn").click(function(){
+		var $has_res = $(".menu").hasClass("active_res");
+		if($has_res == true){
+			$(".menu").removeClass("active_res");
+			$(".menu>ul>li").removeClass("active");
+			$(".menu").stop().slideUp();
+		}else{
+			$(".menu").addClass("active_res");
+			$(".menu").stop().slideDown();
+		}
+		$(".sub_m").hide();
+		return false;
+	});
+
+
+/*마우스 이벤트 영역(PC의 경우)*/
+
+	$(".pc_menu>ul>li").hover(function(){
+		$(".menu>ul>li").removeClass("active");
+		$(this).find(".sub_m").stop().slideDown();
+	}, function(){
+		$(".menu>ul>li").removeClass("active");
+		$(this).find(".sub_m").stop().slideUp();
+	});
 
 
 
